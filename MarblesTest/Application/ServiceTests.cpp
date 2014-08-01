f54440b7da926275ffae04eae6b2b9a0c2a9f660
dcobban <dcobban@shaw.ca>
2014-08-01 04:16:33 -0700

    Re-enabled multi-threaded test

--- MarblesTest/Application/ServiceTests.cpp
+++ MarblesTest/Application/ServiceTests.cpp
@@ -92,30 +92,30 @@ BOOST_AUTO_TEST_CASE( single_thread_test )
 
 BOOST_AUTO_TEST_CASE( multiple_thread_test )
 {
-	//BOOST_MESSAGE( "service.multiple_thread_test" );
-	//const int numCyclesToStop = 1000;
-	//marbles::application app;
-	//marbles::shared_service service = app.start<ExecutedService>();
-	//std::vector<marbles::shared_service> racers;
-	//racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
-	//racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
-	//racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
-	//racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
-	//racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
+	BOOST_MESSAGE( "service.multiple_thread_test" );
+	const int numCyclesToStop = 1000;
+	marbles::application app;
+	marbles::shared_service service = app.start<ExecutedService>();
+	std::vector<marbles::shared_service> racers;
+	racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
+	racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
+	racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
+	racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
+	racers.push_back(app.start<ApplicationStop>(numCyclesToStop));
 
-	//app.run();
+	app.run();
 
-	//BOOST_CHECK(service->provider<ExecutedService>()->executed);
-	//marbles::shared_service winner = *racers.begin();
-	//for(auto i : racers)
-	//{
-	//	if (winner->provider<ApplicationStop>()->count < i->provider<ApplicationStop>()->count)
-	//	{
-	//		winner = i;
-	//	}
-	//}
-	//const int stopCount = winner->provider<ApplicationStop>()->count;
-	//BOOST_CHECK_EQUAL(numCyclesToStop, stopCount);
+	BOOST_CHECK(service->provider<ExecutedService>()->executed);
+	marbles::shared_service winner = *racers.begin();
+	for(auto i : racers)
+	{
+		if (winner->provider<ApplicationStop>()->count < i->provider<ApplicationStop>()->count)
+		{
+			winner = i;
+		}
+	}
+	const int stopCount = winner->provider<ApplicationStop>()->count;
+	BOOST_CHECK_EQUAL(numCyclesToStop, stopCount);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
